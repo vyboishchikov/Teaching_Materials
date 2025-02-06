f6fde3e9b0fa00c5f5a7d5713da3aba99ca8e425
Sergei Vyboishchikov <146196263+vyboishchikov@users.noreply.github.com>
2025-02-06 20:20:56 +0100

    Update Non-Linear-regression.py

--- Non-Linear-regression.py
+++ Non-Linear-regression.py
@@ -1,10 +1,9 @@
 # Non-linear regression for chemical kinetics
-
 import numpy as np
 import scipy.optimize
 import matplotlib.pyplot as plt
 
-# Define the model function for y = A * exp(-k * t**2 - k1*t)
+# Integrated kinetic rate law
 def FirstOrderKinetics(t, Y_inf, Y0, k):
 	return Y_inf + (Y0-Y_inf)*np.exp(-k*t)
 	
@@ -14,8 +13,9 @@ def SecondOrderKinetics(t, Y_inf, Y0, k):
 def ZerothOrderKinetics(t, Y_inf, Y0, k):
 	return Y0 - (Y0-Y_inf)*(k/A0)*t
 
-A0 = 0.01 # initial concentration (non needed for first.order kinetics)
-Function = SecondOrderKinetics # choose the order you need
+Function = SecondOrderKinetics
+
+A0 = 0.01 # define your initial concentration here; not needed for 1st-order kinetics
 
 tData = [ # time data points
   29
@@ -38,26 +38,34 @@ yData = [ # observable (e.g. conductivity) data points
 ]
 
 
-k_init = 1e-12 # initial k value
+if len(tData)>len(yData):
+	print(f"There are {len(tData)} time points, but only {len(yData)} data points.\nCheck your data")
+	quit()
+elif len(tData)<len(yData):
+	print(f"There are {len(yData)} data points, but only {len(tData)} time points.\nCheck your data")
+	quit()
 
-# Perform the non-linear regression using curve_fit
 # Initial guess for parameters Y_inf, Y0, k
+k_init = 1e-2 # initial k value
 InitialGuess = [yData[-1], yData[0], k_init] # initial Y_inf, Y0, k values
 bounds = ([-np.inf for _ in InitialGuess],[np.inf for _ in InitialGuess])
 bounds[0][-1] = 0
 
-# Fit the model to the data
-OptimizedParameters = scipy.optimize.curve_fit(Function, tData, yData, p0=InitialGuess,method='dogbox',bounds=bounds)[0]
+# Non-linear regression
+try:
+	OptimizedParameters, pcov = scipy.optimize.curve_fit(Function, tData, yData, p0=InitialGuess,bounds=bounds,method='dogbox')
+except:
+	print("Fitting failed. Try to change the initial k value")
+	quit()
 
 # Generate fitted curve
 tFit = np.linspace(min(tData), max(tData), 1000)
 yFit = Function(tFit, *OptimizedParameters)
 
-# Print optimized parameters
-print("Optimized parameters (Y_inf, Y0, k):", *OptimizedParameters)
+print('Optimized parameters (Y_inf, Y0, k): '+ ', '.join(['%.3g +- %.1g'%p for p in zip(OptimizedParameters,np.sqrt(np.diag(pcov)))]))
 
-# Plot data and fitted curve
-plt.scatter(tData, yData, label='Data', color='red')
+# Plot
+plt.scatter(tData, yData, label='Data', color='red',s=6)
 plt.plot(tFit, yFit, label='Fitted Curve', color='blue')
 plt.xlabel('t')
 plt.ylabel('y')
